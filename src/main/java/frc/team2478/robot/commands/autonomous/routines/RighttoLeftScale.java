c52a2dff361a34221a621ea018b6358f1e6c7381
Alex Vuong <avuong2962@gmail.com>
2018-03-09 18:03:33 -0700

    Edit sidetooppscale distances

--- src/main/java/frc/team2478/robot/commands/autonomous/routines/RighttoLeftScale.java
+++ src/main/java/frc/team2478/robot/commands/autonomous/routines/RighttoLeftScale.java
@@ -18,16 +18,17 @@ public class RighttoLeftScale extends CommandGroup {
 	
 	public RighttoLeftScale() {
 		addParallel(new LowerHood()); // hood starts raised, lower for safety
-		addSequential(new DriveAuto(216.75)); // drive forward, adjacent to alley between switch and scale
+		addSequential(new DriveAuto(210.75)); // drive forward, adjacent to alley between switch and scale
 		addSequential(new TurnAuto(-90)); // turn left, face down alley
-		addSequential(new DriveAuto(242.25)); // drive through alley, ending up on leftmost side of field
+		addSequential(new DriveAuto(232.25)); // drive through alley, ending up on leftmost side of field
 		addSequential(new TurnAuto(90)); // turn right, face forwards
-		addSequential(new DriveAuto(59)); // drive into nullzone
+		addSequential(new DriveAuto(65)); // drive into nullzone
 		addParallel(new RunShooterAtVelocity(Constants.ShooterRig.AUTO_SPEED)); // rev shooter to save time
 		
 		// if the robot shakes while turning, raiseHood needs to go after AutonomoDriveTurn
 		addParallel(new RaiseHood()); // raise hood to save time
 		addSequential(new TurnAuto(67.5)); // turn right, face the scale
+		addSequential(new WaitCommand(0.5));
 		addParallel(new RunFeedAtDefault()); // rev feed and launch cube
 		addSequential(new WaitCommand(1.5)); // wait for cube to fire
 		addSequential(new StopAllScoringMotors()); // kill motors to save power
